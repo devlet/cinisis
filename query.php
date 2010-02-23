db90d036887a08e620e82f952bd6dd9d542d2d94
Silvio <silvio@devlet.com.br>
2010-02-23 16:34:57 -0300

    Initial db definition for tupi

--- query.php
+++ query.php
@@ -20,7 +20,7 @@ require 'malete/php/Isis.php';
 			));
 
   // Create a db with field list ("fdt")
-  $fdt = array(
+  $fdt_anu10 = array(
 		  'Periodico'            =>  1,
 		  'Data'                 =>  2,
 		  'Titulo'               =>  3,
@@ -41,16 +41,99 @@ require 'malete/php/Isis.php';
 		  'Alimentador'          => 20,
 		  'Tema Anuario'         => 21,
 	      );
+
+  $fdt_tupi = array(
+		  'cod.titulo'                  => 1,
+		  'postopo'                     => 2,
+		  'num.entrada'                 => 3,
+		  'num.tombo'                   => 4,
+		  'datain'                      => 5,
+		  'dataex'                      => 6,
+		  'acervo (DIF ou PRE)'         => 7,
+		  'cadarq'                      => 10,
+		  'caddep'                      => 11,
+		  'cadddl'                      => 12,
+		  'outarq'                      => 13,
+		  'categorias'                  => 15,
+		  'presok'                      => 16,
+		  'roteiro de locução'          => 17,
+		  'mat.acervo'                  => 18,
+		  'mat.orig'                    => 19,
+		  'titulo'                      => 20,
+		  'outros titulos'              => 22,
+		  'data/série'                  => 24,
+		  'conteudo/sinopse'            => 25,
+		  'descritores'                 => 26,
+		  'descritores secundários'     => 27,
+		  'identidades'                 => 28,
+		  'ndxlib'                      => 29,
+		  'materiais'                   => 40,
+		  'materiais'                   => 41,
+		  'materiais'                   => 42,
+		  'materiais'                   => 43,
+		  'materiais'                   => 44,
+		  'materiais'                   => 45,
+		  'mat'                         => 46,
+		  'mat'                         => 47,
+		  'mat'                         => 48,
+		  'mat'                         => 49,
+		  'mat'                         => 50,
+		  'mat'                         => 51,
+		  'mat'                         => 52,
+		  'mat'                         => 53,
+		  'mat'                         => 54,
+		  'mat'                         => 55,
+		  'evol.estado tec.'            => 56,
+		  'movimentacao'                => 57,
+		  'movimentacao'                => 58,
+		  'obsmat'                      => 59,
+		  'producao*'                   => 60,
+		  'dir.arte*'                   => 61,
+		  'fotografia*'                 => 62,
+		  'musica*'                     => 63,
+		  'som*'                        => 64,
+		  'montagem*'                   => 65,
+		  'direcao*'                    => 66,
+		  'arg/roteiro*'                => 67,
+		  'distribuicao*'               => 68,
+		  'colab./outros'               => 69,
+		  'producao1'                   => 70,
+		  'dir.arte1'                   => 71,
+		  'fotografia1'                 => 72,
+		  'musica1'                     => 73,
+		  'som1'                        => 74,
+		  'montagem1'                   => 75,
+		  'producao2(res.p/expandir)'   => 80,
+		  'dir.arte2(res.p/expandir)'   => 81,
+		  'fotografia2(res.p/expandir)' => 82,
+		  'musica2(res.p/expandir)'     => 83,
+		  'som2(res.p/expandir)'        => 84,
+		  'loc.prod.lan'                => 85,
+		  'dat.prod.lan'                => 86,
+		  'lab/est/locacoes'            => 87,
+		  'premios'                     => 88,
+		  'fontes'                      => 89,
+		  'certificados'                => 95,
+		  'examinador'                  => 98,
+		  'observações'                 => 99,
+		  'termos geográficos'          => 165,
+		  'quicktime'                   => 900,
+		  'revisão'                     => 901,
+		  'vídeo'                       => 902,
+		  );
+
+  $fdt = $fdt_tupi;
 ?>
 
 <h2>server</h2>
 
 <?php
-  $db = new Isis_Db($fdt, 'anu10', new Isis_Server());
+  $db = new Isis_Db($fdt, 'tupi', new Isis_Server());
   if (!$db->srv->sock) {
     echo "could not contact server\n";
   }
   else {
+    print_r($db);
 ?>
 
 <h3>terms beginning with...</h3>
@@ -80,7 +163,7 @@ require 'malete/php/Isis.php';
 <h3>query reading a record</h3>
 
 <?php
-    $r = $db->read(522);
+    $r = $db->read(1);
     echo "<pre>---\n", $r->toString(), "---\n</pre><br>\n";
     print_r($r);
   } // end else could contact server
