16032707c55df72fe3260150707eb6f3b488de03
Silvio <silvio@devlet.com.br>
2010-03-29 18:03:25 -0300

    Adding pecl-isis tests

--- test.php
+++ test.php
@@ -179,5 +179,30 @@ include('contrib/spyc/spyc.php');
     //print_r(array_flip($fdt_anu10));
     echo '</pre>';
   } // end else could contact server
+
+?>
+
+<h3>Testing pecl-isis</h3>
+<?php
+
+  //$db = isis_open('db/anu10/anu10');
+  $db = isis_open('db/tupi/tupi');
+  print_r(isis_last_mfn($db));
+
+  echo '<table>';
+
+  $result = isis_search('$', $db);
+  while ($record = isis_fetch_flat_array($result) ) {
+    print (" <tr><td colspan=\"2\">MFN: $record[mfn]</td></tr>\n");
+    for ($i=0; $i<count($record)-1; ++$i) { //-1 porque el mfn es +1
+      list ($tag, $value) = $record[$i];
+      print (" <tr>\n".
+        " <td>$tag</td>\n".
+        " <td>".htmlspecialchars($value)."</td>\n".
+        " </tr>\n");
+    }
+  }
+
+  echo '</table>';
 ?>
 </body></html>
