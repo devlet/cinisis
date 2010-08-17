95cafc843a3dcd9664cec13d2bd0e6c67b29e910
Silvio <silvio@devlet.com.br>
2010-08-17 14:59:35 -0300

    Minor adjusts

--- tests/field.php
+++ tests/field.php
@@ -7,12 +7,13 @@
 require_once '../index.php';
 
 // Get input data.
-$code  = CinisisHttpHelper::get_numeric_arg('code');
 $entry = CinisisHttpHelper::get_numeric_arg('entry');
+$field = CinisisHttpHelper::get_numeric_arg('field');
 
 // Draw the document.
 $display = new CinisisDisplayHelper('Field finder');
-$form    = $display->form_input_text('code', $code) . $display->form_input_text('entry', $entry);
+$form    = $display->form_input_text('entry', $entry);
+$form   .= $display->form_input_text('field', $field);
 $display->form($form, 'field.php');
 
 // Get a db instance.
@@ -21,8 +22,8 @@ $isis = new CinisisDb();
 // Setup database and entry number.
 if ($isis->db) {
   // Get the number of entries.
-  $field   = $isis->db->format['fields'][$code]['name'];
-  $entries = $isis->db->entries();
+  $field_name = $isis->db->format['fields'][$field]['name'];
+  $entries    = $isis->db->entries();
   $entry--;
 
   // Query database.
@@ -31,18 +32,18 @@ if ($isis->db) {
     if ($entry == $entries) {
       break;
     }
-  } while (!isset($result[$field]));
+  } while (!isset($result[$field_name]));
 
   // Navigation bar.
-  $display->navbar($entry, $entries, $repetition, '&code='. $code);
+  $display->navbar($entry, $entries, $repetition, '&field='. $field);
 
   // Format output.
   echo "<pre>\n";
-  echo "Selected field: $code: $field.\n";
+  echo "Selected field: $field: $field_name.\n";
   echo "Showing entry $entry from $entries total entries.\n";
-  echo "Repetitions found: ". count($result[$field]) .".\n";
+  echo "Repetitions found: ". count($result[$field_name]) .".\n";
   echo "\n";
-  print_r($result[$field]);
+  print_r($result[$field_name]);
   echo '</pre>';
 }
 
